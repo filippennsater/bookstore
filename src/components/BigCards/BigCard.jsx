6b48f54758128dc2e2740bfe3c2a10feeebed43a
filippennsater <filip.pennsater@hotmail.com>
2024-02-08 22:25:58 +0100

    renamed component BigCards to BigCard and started function

---
+++ src/components/BigCards/BigCard.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+import { Link } from "react-router-dom";
+import "./BigCard.scss";
+
+function BigCard(props){
+    return(
+        <>
+            <Link>
+            
+            </Link>
+        
+        </>
+    );
+}
+
+export default BigCard;
