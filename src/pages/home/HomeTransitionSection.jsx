4c27e72c927b729e7f57e3b341dd8f41e6c2fac8
filippennsater <filip.pennsater@hotmail.com>
2024-02-09 00:50:05 +0100

    fixing and adding BigCard

--- src/pages/home/HomeTransitionSection.jsx
+++ src/pages/home/HomeTransitionSection.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import '../../App.css';
 import './HomeTransitionSection.scss';
+import BigCard from '../../components/BigCards/BigCard';
 
 
 function HomeTransitionSection() {
@@ -9,6 +10,9 @@ function HomeTransitionSection() {
     return (
 
     <div className='transition-container'>
+
+        <BigCard src='images/exampleImage.png' header='Last months winner!' text='Bigger Project' path='/' />
+        <BigCard src='images/exampleImage.png' header='Last months winner!' text='Bigger Project' path='/' />
             
     </div>
 
