78a2a6c6fe2da963eebb3c730c13521ba53bd212
filippennsater <filip.pennsater@hotmail.com>
2024-02-12 22:37:39 +0100

    fixing styling for BigCard and a filler

--- src/components/BigCards/BigCard.jsx
+++ src/components/BigCards/BigCard.jsx
@@ -5,13 +5,20 @@ import "./BigCard.scss";
 function BigCard(props){
     return(
         <>
-            <Link className="card">
+            <Link className="card" to={props.path}>
                 <div className="card-image-wrap">
                     <img
                     src={props.src}
                     alt="bigboi"
                     className="card-image"
                     />
+                    <div className="big-card-header">
+                        {props.header}
+                    </div>
+
+                    <div className="big-card-text">
+                        {props.text}
+                    </div>
                 </div>
             </Link>
         
--- src/pages/home/HomeTransitionSection.jsx
+++ src/pages/home/HomeTransitionSection.jsx
@@ -14,8 +14,19 @@ function HomeTransitionSection() {
 
         <div className='cards-container'>
 
-        <BigCard src='images/ex1.jpg' header='Last months winner!' text='Bigger Project' path='/' />
-        <BigCard src='images/ex2.jpg' header='Last months winner!' text='Bigger Project' path='/' />
+        <BigCard 
+            src='images/ex1.jpg' 
+            header='Last months winner!' 
+            text='Bigger Project' 
+            path='/' 
+        />
+
+        <BigCard 
+            src='images/ex2.jpg' 
+            header='Last months poetry winner!' 
+            text='In the warmly embracing competition arena stood Elsa Vinterdal, whose beautiful sonnet "Shadows of Euphoria" enchanted the judges and clinched the first place. In second place followed Viktor Månsdotter with his gripping poetry cycle "Hovering Melancholy", while the profound Nils Björnsson took third place with his lyrical ode "The Day That Sings". A heartfelt congratulations to all three for their impressive contributions to the art of poetry!' 
+            path='/' 
+        />
         
         </div>
             
