d638906f3dcf61697b172e99cdd7d52897117d70
filippennsater <filip.pennsater@hotmail.com>
2024-02-15 01:16:40 +0100

    adding images for books and fixing SmallCard

--- src/pages/home/FlowerSection.jsx
+++ src/pages/home/FlowerSection.jsx
@@ -1,7 +1,8 @@
 import React from 'react';
 import '../../App.css';
 import './FlowerSection.scss';
-
+import SmallCard from '../../components/SmallCard/SmallCard';
+import '../../components/SmallCard/SmallCard.scss';
 
 
 function FlowerSection() {
@@ -17,6 +18,38 @@ function FlowerSection() {
                     <div className='fc-line'/>
                     <div className='fc-hb-container'>
                         <fcheader>NEW ARRIVALS</fcheader>
+                    </div>
+
+                    <div className='small-cards-container'>
+
+                        <SmallCard 
+                            src='images/book(1).jpg' 
+                            header='Explore Beatrice Fitzwilliams new book "What If?"' 
+                            text="Intriguing questions about alternate realities and choice" 
+                            path='/Market' 
+                        />
+
+                        <SmallCard 
+                            src='images/book(2).jpg' 
+                            header="Enchanting enigmas and captivating adventures" 
+                            text='' 
+                            path='/' 
+                        />
+
+                        <SmallCard 
+                            src='images/book(3).jpg' 
+                            header='Discover the secrets of craft beer from brewing to tasting!' 
+                            text="" 
+                            path='/Market' 
+                        />
+
+                        <SmallCard 
+                            src='images/book(4).jpg' 
+                            header="A beautifully illustrated tale about transformation and self-discovery" 
+                            text='' 
+                            path='/' 
+                        />
+                    
                     </div>
                     
                 </div>
