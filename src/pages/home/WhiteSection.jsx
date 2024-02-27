a7bdf45b202e2111fdcdd49a03a010c2596041fe
filippennsater <filip.pennsater@hotmail.com>
2024-02-27 21:13:12 +0100

    removing carousel

--- src/pages/home/WhiteSection.jsx
+++ src/pages/home/WhiteSection.jsx
@@ -5,8 +5,7 @@ import './WhiteSection.scss';
 import MediumCard from '../../components/MediumCard/MediumCard';
 import '../../components/MediumCard/MediumCard.scss';
 
-import Carousel from 'react-bootstrap/Carousel';
-import "react-responsive-carousel/lib/styles/carousel.min.css"; // requires a loader
+
 
 
 function WhiteSection() {
@@ -16,46 +15,6 @@ function WhiteSection() {
 
     <div className='white-container'>
 
-        <Carousel>
-
-
-        <Carousel.Item>
-        
-        <div className='medium-cards-container'>
-
-<MediumCard 
-    src='images/book(5).jpg' 
-    header='Elderly Globetrotters' 
-    text="Jasper Wilde" 
-    path='/Market' 
-/>
-
-<MediumCard 
-    src='images/book(6).jpg' 
-    header="Wings" 
-    text="Linnea Berggren"
-    path='/' 
-/>
-
-<MediumCard 
-    src='images/book(7).jpg' 
-    header='Damsel' 
-    text="Clara Müller" 
-    path='/Market' 
-/>
-
-
-
-</div>
-
-
-      </Carousel.Item>
-
-      
-
-
-
-        </Carousel>
             
     </div>
 
