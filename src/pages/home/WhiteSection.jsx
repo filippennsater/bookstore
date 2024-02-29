329fef89a62da0a972bbb34ae794aa34c57e3077
filippennsater <filip.pennsater@hotmail.com>
2024-02-29 19:16:23 +0100

    removed carousel parts in WhiteSection

--- src/pages/home/WhiteSection.jsx
+++ src/pages/home/WhiteSection.jsx
@@ -29,13 +29,7 @@ function WhiteSection() {
       <Slider {...settings}>
         {data.map((d) => (
           <div key={d.name} className="bg-white h-[450px] text-black rounded-xl">
-            <div className='h-56 bg-indigo-500 flex justify-center items-center rounded-t-xl'>
-              <img src={d.img} alt="" className="h-44 w-44 rounded-full"/>
-            </div>
-
-            <div className="flex flex-col items-center justify-center gap-4 p-4">
-              <p className="text-xl font-semibold">{d.name}</p>
-              <p className="text-center">{d.review}</p>
+           
               <button className='bg-indigo-500 text-white text-lg px-6 py-1 rounded-xl'>Read More</button>
             </div>
           </div>
