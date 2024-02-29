669dc979a7f6d1cf731196949707d643b546bf9f
filippennsater <filip.pennsater@hotmail.com>
2024-02-29 19:18:08 +0100

    added data for carousel

--- src/pages/home/WhiteSection.jsx
+++ src/pages/home/WhiteSection.jsx
@@ -29,7 +29,13 @@ function WhiteSection() {
       <Slider {...settings}>
         {data.map((d) => (
           <div key={d.name} className="bg-white h-[450px] text-black rounded-xl">
-           
+            <div className='h-56 bg-indigo-500 flex justify-center items-center rounded-t-xl'>
+              <img src={d.img} alt="" className="h-44 w-44 rounded-full"/>
+            </div>
+
+            <div className="flex flex-col items-center justify-center gap-4 p-4">
+              <p className="text-xl font-semibold">{d.name}</p>
+              <p className="text-center">{d.review}</p>
               <button className='bg-indigo-500 text-white text-lg px-6 py-1 rounded-xl'>Read More</button>
             </div>
           </div>
@@ -43,5 +49,34 @@ function WhiteSection() {
 
     )
 }
+
+const data = [
+  {
+    name: `John Morgan`,
+    img: `/students/John_Morgan.jpg`,
+    review: `Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.`
+  },
+  {
+    name: `Ellie Anderson`,
+    img: `/students/Ellie_Anderson.jpg`,
+    review: `Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.`
+  },
+  {
+    name: `Nia Adebayo`,
+    img: `/students/Nia_Adebayo.jpg`,
+    review: `Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.`
+  },
+  {
+    name: `Rigo Louie`,
+    img: `/students/Rigo_Louie.jpg`,
+    review: `Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.`
+  },
+  {
+    name: `Mia Williams`,
+    img: `/students/Mia_Williams.jpg`,
+    review: `Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.`
+  },
+  
+];
 
 export default WhiteSection
