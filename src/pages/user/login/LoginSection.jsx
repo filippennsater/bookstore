a2d0cf401af349aaca09318198dc98fea347a6e6
filippennsater <filip.pennsater@hotmail.com>
2024-04-09 22:21:16 +0200

    fixed a container for the form

--- src/pages/user/login/LoginSection.jsx
+++ src/pages/user/login/LoginSection.jsx
@@ -6,8 +6,12 @@ function LoginSection() {
     return (
 
         <div className='login-container'>
-            
-            <p>Login</p>
+
+            <div className='login-form-container'>
+
+
+
+            </div>
 
         </div>
 
