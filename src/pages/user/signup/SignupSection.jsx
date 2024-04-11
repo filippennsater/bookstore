d70cf7d5d8bd7696473338de367e68e83e3ef3ff
filippennsater <filip.pennsater@hotmail.com>
2024-04-12 00:12:56 +0200

    fixing the SignupSection

--- src/pages/user/signup/SignupSection.jsx
+++ src/pages/user/signup/SignupSection.jsx
@@ -6,8 +6,31 @@ function SignupSection() {
     return (
 
         <div className='signup-container'>
-            
-            <p>Signup</p>
+
+            <div className='signup-form-container'>
+
+                <div className='profile-form-header'>
+                    Signup
+                </div>
+
+                <form>
+                    <label>
+                        Name<br />
+                        <input className='input-signup' type="text" name="name" /><br />
+                    </label>
+                    <label>
+                        Email<br />
+                        <input className='input-signup' type="text" name="name" /><br />
+                    </label>
+                    <label>
+                        Password<br />
+                        <input className='input-signup' type="text" name="name" /><br /><br />
+                    </label>
+
+                    <input type="submit" value="Signup" />
+                </form>
+
+            </div>
 
         </div>
 
