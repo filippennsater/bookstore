59dbad1bd742aab1b8b061dbfcff0c6724076ab1
filippennsater <filip.pennsater@hotmail.com>
2024-04-21 01:30:27 +0200

    removing the email from the login page

--- src/pages/user/login/LoginSection.jsx
+++ src/pages/user/login/LoginSection.jsx
@@ -1,9 +1,9 @@
 import React from 'react';
 import '../../../App.css';
 import './LoginSection.scss';
-import { useState, useEffect} from 'react';
+import { useState/*, useEffect*/ } from 'react';
 import { auth } from '../../../firebase.js';
-import { onAuthStateChanged, signInWithEmailAndPassword } from 'firebase/auth';
+import { /*onAuthStateChanged, */signInWithEmailAndPassword } from 'firebase/auth';
 
 function LoginSection() {
 
@@ -12,25 +12,30 @@ function LoginSection() {
 
     const handleLogin = async () => {
         try {
-          const user = await signInWithEmailAndPassword(
-            auth,
-            loginEmail,
-            loginPassword
-          );
-          console.log(user);
+            const user = await signInWithEmailAndPassword(
+                auth,
+                loginEmail,
+                loginPassword
+            );
+            console.log(user);
         } catch (error) {
-          console.log(error.message);
+            console.log(error.message);
         }
-      };
+    };
 
-      const [user, setUser] = useState({});
+    //const [user, setUser] = useState({});
 
-      useEffect(() => {
+    /*
+    useEffect(() => {
         onAuthStateChanged(auth, (currentUser) => {
             setUser(currentUser);
         });
-    
+
     }, [])
+    
+    Can be used with:
+    <h4> {user ? user.email : "Not Logged In"} </h4>
+    */
 
     return (
 
@@ -50,9 +55,9 @@ function LoginSection() {
                     </div>
                     <div className='login-input-wrap'>
                         <input
-                           onChange={(event) => {
-                            setLoginEmail(event.target.value);
-                          }}
+                            onChange={(event) => {
+                                setLoginEmail(event.target.value);
+                            }}
                             className='input-login'
                             type="email"
                             name="email"
@@ -66,7 +71,7 @@ function LoginSection() {
                         <input
                             onChange={(event) => {
                                 setLoginPassword(event.target.value);
-                              }}
+                            }}
                             className='input-login'
                             type="password"
                             name="password"
@@ -80,8 +85,7 @@ function LoginSection() {
                 <div className='login-info-text'>
                     Please note: For demo login with usr: test@gmail.com and pwd: test123
                 </div>
-                <h4> {user ? user.email : "Not Logged In"} </h4>
-                 
+
 
             </div>
 
