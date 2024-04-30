3ec6479ac56e2ce3bd239f3965ff048863686be6
filippennsater <filip.pennsater@hotmail.com>
2024-04-30 22:51:42 +0200

    just making things nicer in document

--- src/components/StaticCard/StaticCard.jsx
+++ src/components/StaticCard/StaticCard.jsx
@@ -2,28 +2,28 @@ import React from "react";
 import { Link } from "react-router-dom";
 import "./StaticCard.scss";
 
-function StaticCard(props){
-    return(
+function StaticCard(props) {
+    return (
         <>
             <Link className="static-card" to={props.path}>
+
                 <div className="static-card-image-wrap">
                     <img
-                    src={props.src}
-                    alt="smallWboi"
-                    className="static-card-image"
+                        src={props.src}
+                        alt="smallWboi"
+                        className="static-card-image"
                     />
                 </div>
 
-                    <div className="static-card-header">
-                        {props.header}
-                    </div>
+                <div className="static-card-header">
+                    {props.header}
+                </div>
 
-                    <div className="static-card-text">
-                        {props.text}
-                    </div>
+                <div className="static-card-text">
+                    {props.text}
+                </div>
                 
             </Link>
-        
         </>
     );
 }
