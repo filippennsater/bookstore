3a4defab2dee1b3b0c7ec7b578f7fb95398a1da3
filippennsater <filip.pennsater@hotmail.com>
2024-05-14 23:57:45 +0200

    fixing ProductItem with more props

--- src/components/products/ProductItem.jsx
+++ src/components/products/ProductItem.jsx
@@ -4,7 +4,10 @@ const ProductItem = ({item}) => {
   return (
     <div>
         <img src={item.image} alt={item.title} />
-        
+        <h3>{item.title}</h3>
+        <p>{item.price}</p>
+        <p>{item.qtyInStock}</p>
+        <p>{item.description}</p>
     </div>
   )
 }
