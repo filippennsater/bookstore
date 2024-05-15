4deaa3b29b02b5a08e4a1a38215358ddc08dbbfd
filippennsater <filip.pennsater@hotmail.com>
2024-05-15 21:17:18 +0200

    changing some names for files in the shop

--- src/pages/shop/Shop.jsx
+++ src/pages/shop/Shop.jsx
@@ -1,12 +1,12 @@
 import React from 'react'
 import '../../App.css'
-import BooksSection from './BooksSection'
+import ShopSection from './ShopSection'
 import Footer from '../../components/footer/Footer';
 
 function Shop (){
     return (
     <>
-      <BooksSection/>
+      <ShopSection/>
       <Footer/>
     </>
     );
---
+++ src/pages/shop/ShopSection.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import '../../App.css';
+import './ShopSection.scss';
+
+function ShopSection() {
+    return (
+
+        <div className='shop-container'>
+            
+
+        </div>
+
+    )
+}
+
+export default ShopSection
