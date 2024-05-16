fc0f76f65c710315f644c220112a2b7526ccf526
filippennsater <filip.pennsater@hotmail.com>
2024-05-16 23:26:08 +0200

    fixing ShopSection by importing the products

--- src/pages/shop/ShopSection.jsx
+++ src/pages/shop/ShopSection.jsx
@@ -1,12 +1,14 @@
 import React from 'react';
 import '../../App.css';
 import './ShopSection.scss';
+import products from '../../components/products/products'
 
 function ShopSection() {
     return (
 
         <div className='shop-container'>
             
+            <h1>Products</h1>
 
         </div>
 
