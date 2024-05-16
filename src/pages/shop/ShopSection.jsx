8ffcc870ca8d3da201fbbb53fcbdf4565e971d2d
filippennsater <filip.pennsater@hotmail.com>
2024-05-16 23:43:57 +0200

    displaing the different products now in ShopSection

--- src/pages/shop/ShopSection.jsx
+++ src/pages/shop/ShopSection.jsx
@@ -1,14 +1,18 @@
 import React from 'react';
 import '../../App.css';
 import './ShopSection.scss';
-import products from '../../components/products/products'
+import { products } from '../../components/products/products'
+import ProductItem from '../../components/products/ProductItem';
 
 function ShopSection() {
     return (
 
         <div className='shop-container'>
-            
+
             <h1>Products</h1>
+            {products.map((item) => (
+                <ProductItem item={item} />
+            ))}
 
         </div>
 
