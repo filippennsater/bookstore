aafc7b0836b63c92f3da6e81bfe9bd8080c43e74
filippennsater <filip.pennsater@hotmail.com>
2024-05-29 00:34:23 +0200

    adjusted classnames to be easier to understand

--- src/pages/singleItem/SingleItemSection.jsx
+++ src/pages/singleItem/SingleItemSection.jsx
@@ -1,28 +1,31 @@
 import React from "react";
-import styles from "./SingleItemSection.module.css";
+import "./SingleItemSection.scss";
 
 import { connect } from "react-redux";
 import { addToCart } from "../../redux/Shopping/shopping-actions";
 
 const SingleItemSection = ({ current, addToCart }) => {
-  return (
-    <div className={styles.singleItem}>
-      <img
-        className={styles.singleItem__image}
-        src={current.image}
-        alt={current.title}
-      />
-      <div className={styles.singleItem__details}>
-        <p className={styles.details__title}>{current.title}</p>
-        <p className={styles.details__description}>{current.description}</p>
-        <p className={styles.details__price}>$ {current.price}</p>
 
-        <button
-          onClick={() => addToCart(current.id)}
-          className={styles.details__addBtn}
-        >
-          Add To Cart
-        </button>
+  return (
+    <div className='SingleItem-container'>
+      <div className='SingleItem'>
+        <img
+          className='singleItem-image'
+          src={current.image}
+          alt={current.title}
+        />
+        <div className='singleItem-details'>
+          <div className='details-title'>{current.title}</div>
+          <div className='details-description'>{current.description}</div>
+          <div className='details-price'>$ {current.price}</div>
+
+          <button
+            onClick={() => addToCart(current.id)}
+            className='details-addBtn'
+          >
+            Add To Cart
+          </button>
+        </div>
       </div>
     </div>
   );
