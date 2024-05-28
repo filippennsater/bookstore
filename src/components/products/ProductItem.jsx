b16ba5f5849e7eaf46d916844956c62ee77b187d
filippennsater <filip.pennsater@hotmail.com>
2024-05-28 19:48:17 +0200

    fixed so every product can generate their own page

--- src/components/products/ProductItem.jsx
+++ src/components/products/ProductItem.jsx
@@ -1,19 +1,22 @@
 import React from 'react'
 import "./ProductItem.scss";
-//import { Link } from 'react-router-dom';
+import { Link } from 'react-router-dom';
 
-import {connect} from 'react-redux';
-import { addToCart } from "../../redux/Shopping/shopping-actions";
+import { connect } from 'react-redux';
+import { loadCurrentItem, addToCart } from "../../redux/Shopping/shopping-actions";
 
-const ProductItem = ({item, addToCart}) => {
+
+const ProductItem = ({ item, addToCart, loadCurrentItem }) => {
   return (
     <div className='product-item-container'>
+      <Link to={`/product/${item.id}`} onClick={() => loadCurrentItem(item)}>
         <img className='product-image' src={item.image} alt={item.title} />
         <div className='product-title'>{item.title}</div>
-        <div className='product-price'>{item.price} kr</div>
-        <div className='product-qty'>{item.qtyInStock}</div>
-        <div className='product-text'>{item.description}</div>
-        <button className='buy-button' onClick={() => addToCart(item.id)}>Add To Cart</button>
+      </Link>
+      <div className='product-price'>{item.price} kr</div>
+      <div className='product-qty'>{item.qtyInStock}</div>
+      <div className='product-text'>{item.description}</div>
+      <button className='buy-button' onClick={() => addToCart(item.id)}>Add To Cart</button>
     </div>
   )
 }
@@ -21,7 +24,7 @@ const ProductItem = ({item, addToCart}) => {
 const mapDispatchToProps = (dispatch) => {
   return {
     addToCart: (id) => dispatch(addToCart(id)),
-    //loadCurrentItem: (item) => dispatch(loadCurrentItem(item)),
+    loadCurrentItem: (item) => dispatch(loadCurrentItem(item)),
   };
 };
 
