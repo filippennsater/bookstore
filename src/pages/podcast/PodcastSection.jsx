efa53eb6c3e70186ae8dd20991987306c04b0bdc
filippennsater <filip.pennsater@hotmail.com>
2024-06-03 17:32:47 +0200

    fixed pictures for my podcasts, also started some more structure for the view

--- src/pages/podcast/PodcastSection.jsx
+++ src/pages/podcast/PodcastSection.jsx
@@ -2,10 +2,20 @@ import React from 'react';
 import '../../App.css';
 import './PodcastSection.scss';
 
+import { useParams } from 'react-router-dom';
+import { podcasts } from '../../components/StaticCard/Podcasts';
+
 function PodcastSection() {
+
+    const { id } = useParams();
+    const podcast = podcasts.find(podcast => podcast.id === parseInt(id));
+
     return (
 
         <div className='podcast-container'>
+
+            <h1>{podcast.title}</h1>
+            <h2>Guest: {podcast.guest}</h2>
 
         </div>
 
