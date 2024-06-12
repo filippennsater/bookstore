8aa368edd92060c931ed3ee1869f363d91906f0c
filippennsater <filip.pennsater@hotmail.com>
2024-06-12 02:39:17 +0200

    adjusted the links in the footer

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -35,9 +35,9 @@ function Footer() {
 
                     <div className='footer-link-items'>
                         <h2>Rights</h2>
-                        <Link to='https://github.com/filippennsater/portfolio' target='_blank'>Open source</Link>
-                        <Link to='/'>AI created media</Link>
-                        <Link to='/'>Terms of service</Link>
+                        <Link to='https://opensource-7d53e.web.app' target='_blank'>Open source</Link>
+                        <Link to='https://mediainformation-ca3b9.web.app' target='_blank'>AI created media</Link>
+                        <Link to='https://userinformation-76154.web.app' target='_blank'>ToS and PP</Link>
                     </div>
 
                     <div className='footer-link-items'>
@@ -56,7 +56,7 @@ function Footer() {
             <section className='social-media'>
                 <div className='social-media-wrap'>
                     <div className='footer-logo'>
-                        <Link to='/' className='footer-logo'/>
+                        <Link to='/' className='footer-logo' />
                     </div>
                     <small className='website-rights'>© 2023</small>
                     <div className='social-icons'>
